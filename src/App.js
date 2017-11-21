51553b88c780f1aad6ca1e9a84ec0abdc414e6d1
Trajche <roskoskitrajce@gmail.com>
2017-11-21 22:17:38 +0100

    added top react components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchBar />
+        <WeatherList />
       </div>
     );
+  }
+}
+
+class SearchBar extends Component {
+  render(){
+    return (
+      <div class="row">
+      <div class="col-sm-10">
+        <input type="text" class="form-control" style={{marginBottom:'20px'}} placeholder="Search for a city ..." />
+      </div>
+      <div class="col-sm-2">
+        <DegreeSwitcher />
+      </div>
+      </div>
+    );
+  }
+}
+
+class DegreeSwitcher extends Component {
+  render(){
+    return (
+      <div class="btn-group">
+          <button type="button" class="btn btn-default">°C</button>
+          <button type="button" class="btn btn-default">°F</button>
+      </div>
+    );
+  }
+}
+
+class WeatherList extends Component {
+  render(){
+    return (
+      <div class="list" style={{background: 'white'}}>
+        <div class="col-sm-6" style={{background:'white'}}>
+            <div class="blue-grey-700">
+                <span style={{fontSize:'30px'}}>City, </span>
+                <span style={{fontSize:'24px'}}>Country</span>
+              </div>
+              <div>
+                <span class="mr-5">Today,</span> 05:12 PM
+              </div>
+        </div>
+        <div class="col-sm-3" style={{background:'#34495e'}}>
+          <div style={{color:'white'}}>
+            <i class="wb-chevron-up red-500 font-size-20 mr-5"></i>
+            <span style={{fontSize:'30px'}}>8°</span>
+            <span style={{fontSize:'24px'}}>C</span>
+            </div>
+          <div style={{fontSize:'14px'}}>LOW</div>
+        </div>
+        <div class="col-sm-3" style={{background:'#2c3e50'}}>
+            <div style={{color:'white'}}>
+              <i class="wb-chevron-up red-500 font-size-20 mr-5"></i>
+              <span style={{fontSize:'30px'}}>19°</span>
+              <span style={{fontSize:'24px'}}>C</span>
+              </div>
+            <div style={{fontSize:'14px'}}>HIGH</div>
+        </div>
+      </div>
+      );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './App';
+
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/"  >
+      <IndexRoute component={App} />
+      <Route path="city/:name" component={App}/>
+    </Route>
+  </Router>
+), document.getElementById('root'));
+
 registerServiceWorker();
