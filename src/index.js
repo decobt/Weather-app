17076df110f5a8a49216c7d616efbd83ea21226a
Trajche <roskoskitrajce@gmail.com>
2017-11-22 21:28:11 +0100

    added sample data

--- src/index.js
+++ src/index.js
@@ -8,11 +8,17 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 import registerServiceWorker from './registerServiceWorker';
 
+var data = {
+  '0':{'country':'Macedonia','city':'Skopje', 'temp':{'low':'15', 'high':'28'}},
+  '1':{'country':'England','city':'London', 'temp':{'low':'10', 'high':'15'}},
+  '2':{'country':'Spain','city':'Barcelona', 'temp':{'low':'18', 'high':'34'}},
+  '3':{'country':'France','city':'Paris', 'temp':{'low':'13', 'high':'26'}},
+};
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/"  >
-      <IndexRoute component={App} />
+      <IndexRoute component={App} data={data} />
       <Route path="city/:name" component={App}/>
     </Route>
   </Router>
