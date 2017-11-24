0178b3ddde3b9c2b4e7c4c9e2fe2e0668e6dadee
Trajche <roskoskitrajce@gmail.com>
2017-11-24 22:25:44 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import './App.css';
 
+//main App component
 class App extends Component {
   constructor(props){
     super(props);
@@ -9,35 +11,50 @@ class App extends Component {
       searchText:'',
       degree: 'C'
     }
+    //bind all events
     this.OnSearchText = this.OnSearchText.bind(this);
     this.CelsiusChange = this.CelsiusChange.bind(this);
     this.FarenheitChange = this.FarenheitChange.bind(this);
   }
+  //update state when user enters text in searchbar
   OnSearchText(e){
     this.setState({
       searchText:e.target.value
     });
   }
+  //fire event when user clicks on celsius button, calculate new temp values
   CelsiusChange(){
+    //first check if temp are in Farenheit
     if(this.state.degree ==='F'){
       var newdata = this.state.data;
+      //loop through the data
       for(var i in newdata){
+        //calculate low temperature
         newdata[i].temp.low = ((newdata[i].temp.low - 32) / 1.8).toFixed(0);
+        //calculate high temperature
         newdata[i].temp.high = ((newdata[i].temp.high - 32) / 1.8).toFixed(0);
       }
+      //update the state with the new data
       this.setState({
         data:newdata,
         degree: 'C'
       });
     }
   }
+
+  //fire event when user clicks on farenheit button, calculate new temp values
   FarenheitChange(){
+    //first check if temp is in celsius
     if(this.state.degree ==='C'){
       var newdata = this.state.data;
+      //loop through the data
       for(var i in newdata){
+        //calculate new value for low temperature
         newdata[i].temp.low = Math.round(newdata[i].temp.low * 1.8 + 32);
+        //calculate new value for high temperature
         newdata[i].temp.high = Math.round(newdata[i].temp.high * 1.8 + 32);
       }
+      //update the state with new values
       this.setState({
         data:newdata,
         degree: 'F'
@@ -46,24 +63,33 @@ class App extends Component {
   }
   render() {
     var rows = [];
+    //loop through each record in the data
     for(var i in this.state.data){
+      //check if searchtext is not set
       if(this.state.searchText===''){
-          rows.push( <WeatherList key={i} data={this.state.data[i]} degree={this.state.degree} /> );
+        //if it is not, push each row
+        rows.push( <WeatherList key={i} data={this.state.data[i]} degree={this.state.degree} /> );
       }else{
+        //if searchtext is set, compare searchText with city name
         if(this.state.data[i].city.toLowerCase().indexOf(this.state.searchText)>= 0){
+          //if they match, push that record
           rows.push( <WeatherList key={i} data={this.state.data[i]} degree={this.state.degree} /> );
         }
       }
     }
+    //render component
     return (
       <div className="App">
         <SearchBar onSearch={this.OnSearchText} onCelClick={this.CelsiusChange} onFarClick={this.FarenheitChange}/>
         {rows}
+        <p className="footer">Weather App, Created by Trayche Roshkoski</p>
       </div>
     );
   }
 }
 
+
+// Searchbar component (search field plus measure conversion button)
 class SearchBar extends Component {
   render(){
     return (
@@ -79,6 +105,7 @@ class SearchBar extends Component {
   }
 }
 
+//measure conversion buttons used to convert the degrees between C and F
 class DegreeSwitcher extends Component {
   render(){
     return (
@@ -90,14 +117,17 @@ class DegreeSwitcher extends Component {
   }
 }
 
+//weather list row, consists of basic info, low and high temp components
 class WeatherList extends Component {
   render(){
     return (
+      <Link to={'city/'+this.props.data.city} className="cityLink">
       <div className="list clearfix">
-        <PlaceInfo name={this.props.data.city} country={this.props.data.country} />
+        <PlaceInfo name={this.props.data.city} country={this.props.data.country} icon={this.props.data.icon} />
         <LowTemp temp={this.props.data.temp.low} degree={this.props.degree}  />
         <HighTemp temp={this.props.data.temp.high} degree={this.props.degree}/>
       </div>
+      </Link>
       );
   }
 }
@@ -108,7 +138,7 @@ class PlaceInfo extends Component {
     return(
       <div>
       <div className="col-sm-2">
-        <Chevron font="48px" klasa="wi wi-day-sunny icon" />
+        <Chevron font="48px" klasa={this.props.icon+' icon'} />
       </div>
       <div className="col-sm-6" style={{background:'white', padding:'15px 0px'}}>
         <div>
@@ -124,6 +154,7 @@ class PlaceInfo extends Component {
   }
 }
 
+//low temperature component
 class LowTemp extends Component{
   render(){
     return (
@@ -139,6 +170,7 @@ class LowTemp extends Component{
   }
 }
 
+//high temperature component
 class HighTemp extends Component{
   render(){
     return(
@@ -154,6 +186,7 @@ class HighTemp extends Component{
   }
 }
 
+//icon component
 class Chevron extends Component{
   render(){
     return(
@@ -162,4 +195,5 @@ class Chevron extends Component{
   }
 }
 
+//export the App component
 export default App;
